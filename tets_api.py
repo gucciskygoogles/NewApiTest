66ab28a87447f14148cbf8bef3e48435acc8f01e
Nick <iwillloveyou44@yandex.ru>
2022-01-16 17:36:58 +0300

    Second

--- tets_api.py
+++ tets_api.py
@@ -1,8 +1,9 @@
 import requests
 import config
 import pytest
+import unittest
 
-class APITest:
+class APITest(unittest.TestCase):
     def test_get_status(self):
-        response = requests.get(f'api.openweathermap.org/data/2.5/weather?q=London&appid={config.APIkey}')
+        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=London&appid={config.APIkey}')
         assert response.status_code == 200
