b704d31ae6d979c5f35aa941a2e0da824ac615b3
Nick <iwillloveyou44@yandex.ru>
2022-01-31 17:01:15 +0300

    allure

--- main_dir/conftest.py
+++ main_dir/conftest.py
@@ -23,3 +23,16 @@ def response_first():
     response_first = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=London&appid={config.APIkey}')
     return response_first
 
+@pytest.fixture()
+def register():
+    user_to_register = {
+        "email": "eve.holt@reqres.in",
+        "password": "pistol"
+    }
+    return user_to_register
+
+@pytest.fixture()
+def cookies(register):
+    response = requests.post("https://reqres.in/api/users/", data=register)
+    cookies = response.cookies.get("__stripe_mid")
+    return cookies
--- main_dir/test_api.py
+++ main_dir/test_api.py
@@ -1,29 +1,41 @@
 import pytest
 import requests
 from json import JSONDecoder
+import allure
 
+
+@allure.epic('Weather Cases')
 class TestWeatherAPITest:
 
+
+    @allure.description('Get correct status')
     @pytest.mark.get
     @pytest.mark.xfail
     def test_get_status(self, response_first):
-        assert response_first.status_code == 201
+        with allure.step('Получение статуса кода'):
+            assert response_first.status_code == 201
+
 
     @pytest.mark.get
     def test_get_json(self, response_first):
         try:
-            assert response_first.headers['Content-type'] == 'application/json; charset=utf-8'
+            with allure.step('Проверка на формат данных'):
+                assert response_first.headers['Content-type'] == 'application/json; charset=utf-8'
         except JSONDecoder:
             print('Not JSON format')
+
 
     @pytest.mark.get
     def test_city_correct_location(self, response_first):
         try:
             response_body = response_first.json()
-            assert 'coord' in response_body
-            assert len(response_body['coord']) == 2
+            with allure.step('Проверка наличия имени в JSON'):
+                assert 'coord' in response_body
+            with allure.step('Проверка длинны'):
+                assert len(response_body['coord']) == 2
         except JSONDecoder:
             print('Not JSON format')
+
 
     @pytest.mark.get
     def test_correct_city_name(self, response_first):
@@ -34,7 +46,10 @@ class TestWeatherAPITest:
         except JSONDecoder:
             print('Not JSON format')
 
+
+@allure.epic('Reqres Cases')
 class TestReqresInAPItest:
+
 
     @pytest.mark.post
     def test_create_user(self, post_method):
@@ -44,6 +59,7 @@ class TestReqresInAPItest:
         except JSONDecoder:
             print('Not JSON format')
 
+
     @pytest.mark.update
     def test_update_user(self, put_method):
         try:
@@ -52,12 +68,24 @@ class TestReqresInAPItest:
         except JSONDecoder:
             print('Not JSON format')
 
+
     @pytest.mark.parametrize('users', [i for i in range(1, 11)])
     def test_get_name_of_user(self, users):
         try:
             response = requests.get(f"https://reqres.in/api/users/{users}")
             response_body = response.json()
+            assert 'data' in response_body
             print(response_body['data']['first_name'])
             assert response.status_code == 200
         except JSONDecoder:
             print('Not JSON format')
+
+
+    @pytest.mark.post
+    def test_succesful_register(self, register, cookies):
+        response = requests.post("https://reqres.in/api/users/", data=register)
+        assert response.status_code == 201
+        assert "token" in response.json(), 'No token in data'
+        assert "__stripe_mid" in cookies
+
+
